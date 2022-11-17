1999aa67db6929e1764f2693eec078b77b574dd2
echen4628 <echen4628@gmail.com>
2022-11-17 10:52:10 -0800

    start filter
    Co-authored-by: Erina Iwasa <eiwasa@users.noreply.github.com>

--- FASTSlam.py
+++ FASTSlam.py
@@ -1,9 +1,10 @@
 import numpy as np
 class Particle():
     def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.yaw = 0 
+        self.state = np.zeros(3)
+        # self.x = 0
+        # self.y = 0
+        # self.yaw = 0 
         self.landmark = np.zeros((15,2))
         #[[1,2],
         #  [2,3]]
@@ -11,16 +12,42 @@ class Particle():
         #[ [[1,2],
         #    [3,4]],
         #   [[]]]
+    def get_x(self):
+        return self.state[0]
+    def get_y(self):
+        return self.state[1]
+    def get_yaw(self):
+        return self.state[2]
+    def set_x(self, x):
+        self.state[0] = x
+    def set_y(self, y):
+        self.state[1] = y
+    def set_yaw(self, yaw):
+        self.state[2] = yaw
+
+    def __repr__(self):
+        return f"X: {self.get_x} \n \
+                 Y: {self.get_y} \n \
+                 yaw: {self.get_yaw} \n \
+                 landmark: {self.landmark} \n \
+                 landmark_cov: {self.landmark_cov} \n \
+                ------------------------"
         
-class FASTSlam():
+class Fastslam():
     def __init__(self, num_particles):
-        # self.particles = lists of particles (not numpy list)
-        # self.weights = 
-        # self.next_particles = lists of particles
-        # self.next_weights
-        # weighted_particle = particle
-        pass
+        self.num_particles = num_particles
+        self.particles = [Particle() for i in range(num_particles)]
+        self.weights = np.ones(num_particles)*(1/num_particles)
+        self.next_particles = [Particle() for i in range(num_particles)]
+        self.next_weights = np.ones(num_particles)*(1/num_particles)
+        self.weighted_particle = Particle()
     
+    def __repr__(self):
+        return f"num_particles: {self.num_particles}\n \
+                 particles: {self.particles[:3]}\n \
+                 weights: {self.weights[:3]}\n \
+                 weighted_particle: {self.weighted_particle}"
+
     def prediction(self):
         # propagate and add noise using the self.particles
         #  and store in self.next_particles
@@ -28,15 +55,43 @@ class FASTSlam():
         # self.next_particles will be updated
         pass
     
-    def propagate_state(self):
+    def propagate_all_states(self, u_t_noiseless, dt):
+        
         pass
+
+    def propogate_single_particle_state(x_t_prev, u_t_noiseless, delta_t):
+        """
+        Propagate/predict the state based on chosen motion model
+
+        Parameters:
+        x_t_prev (np.array)  -- the previous state estimate
+        u_t (np.array)       -- the current control input (odometry [x_dot, yaw_dot])
+
+        Returns:
+        x_bar_t (np.array)   -- the predicted state
+        """
+        pass
+        x_bar_t = np.zeros((3))
+
+
+        x_bar_t[0:, 0] = x_t_prev[0:, 0] + x_t_prev[0:, 2]*delta_t # x
+        x_bar_t[0:, 0] = x_t_prev[0:, 0] + u_t_noiseless[0]*np.cos(x_t_prev[0:, 4])*delta_t
+        
+        
+        x_bar_t[0:, 1] = x_t_prev[0:, 1] + x_t_prev[0:, 3]*delta_t # y
+        x_bar_t[0:, 2] = x_t_prev[0:, 2] + u_t_noiseless[0] * \
+        np.cos(x_t_prev[0:, 4])*delta_t  # needs to be in radian
+        x_bar_t[:, 3] = x_t_prev[:, 3] + u_t_noiseless[0]*  \
+        np.sin(x_t_prev[:, 4])*delta_t
+        x_bar_t[:, 4] = x_t_prev[:, 4] + u_t_noiseless[1]*delta_t # yaw
+        # x_bar_t[0:, 5] = x_t_prev[0:, 5] # weight
 
 #    def propogate_state(x_t_prev, u_t_noiseless):
 #     """Propagate/predict the state based on chosen motion model
 
 #     Parameters:
 #     x_t_prev (np.array)  -- the previous state estimate
-#     u_t (np.array)       -- the current control input
+#     u_t (np.array)       -- the current control input (odometry)
 
 #     Returns:
 #     x_bar_t (np.array)   -- the predicted state
@@ -55,8 +110,6 @@ class FASTSlam():
 # """STUDENT CODE END"""
 #      return x_bar_t
 
-
-    
     def reweight(self, z_t, sigma_z_t):
         # reweight this self.next_particles
         pass
@@ -72,3 +125,7 @@ class FASTSlam():
     def resample(self):
         # resample from self.next_particles and then reassign self.particles
         pass
+
+if __name__ == "__main__":
+    num_particles = 3
+    test = Fastslam(num_particles)
