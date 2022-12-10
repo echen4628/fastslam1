fixed correction step z_diff sign, swapped landmark coordinates
Co-authored-by: Erina Iwasa <eiwasa@users.noreply.github.com>

FASTSlam.py
@@ -89,7 +89,7 @@ class Fastslam():
         # print(u_t_noiseless)
         for i in range(len(self.particles)):
             self.particles[i].state = self.propogate_single_particle_state(self.particles[i].state, u_t_noiseless, dt)
-            x_t_var = np.array([0.2,0.2,0.1])
+            x_t_var = np.array([0.1,0.1,0.05])
             x_t_noise_x = np.random.normal(0, x_t_var[0])
             x_t_noise_y = np.random.normal(0, x_t_var[1])
             x_t_noise_z = np.random.normal(0, x_t_var[2])
@@ -145,13 +145,14 @@ class Fastslam():
                     # find difference between estimation/pred and measurement z - z hat
                     z_diff = np.zeros((2,1))
                     H_t = np.identity(2)
-                    z_diff[0,0] = current_landmark[0] - measured_landmark_x
-                    z_diff[1,0] = current_landmark[1] - measured_landmark_y
+                    z_diff[0,0] = measured_landmark_x - current_landmark[0]
+                    z_diff[1,0] = measured_landmark_y - current_landmark[1] 
                     # find Modified_ measurement covariance H*Cov_landmark*H^T + Q <-identity.
-                    sensor_cov = np.identity(2)
+                    sensor_cov = np.identity(2)/10
+                    # sensor_cov = np.zeros((2,2))
                     modified_measurement_cov = self.calc_measurement_cov(current_landmark_cov, sensor_cov)
                     # find w*
-                    new_w = max(0.0000001, self.calc_weight(modified_measurement_cov, z_diff))
+                    new_w = max(0.000000001, self.calc_weight(modified_measurement_cov, z_diff))
                     # new_w = self.calc_weight(modified_measurement_cov, z_diff)
                     self.weights[idx] =  self.weights[idx] * new_w
                     # call the update function to update landmark of particle p
@@ -164,12 +165,14 @@ class Fastslam():
         self.weights = self.weights/(np.sum(self.weights))
 
     def update(self, modified_measurment_cov, landmark_cov, H_t, current_landmark, z_diff):
+        # pdb.set_trace()
         K_t = landmark_cov@H_t.T@np.linalg.inv(modified_measurment_cov)
         updated_landmark = current_landmark + (K_t@z_diff).reshape(2) # 2
         updated_landmark_cov = (np.identity(2) - K_t@H_t)@landmark_cov # 2x2
         return updated_landmark, updated_landmark_cov
     
     def calc_weight(self, modified_measurement_cov, z_diff):
+        # print(modified_measurement_cov)
         inverted_cov_term = np.linalg.inv(modified_measurement_cov)
         exp_term = -0.5*z_diff.T@inverted_cov_term@z_diff
         weight_coefficient = np.linalg.det(2*np.pi*modified_measurement_cov)**(-0.5)
@@ -192,6 +195,7 @@ class Fastslam():
         return measured_landmark_x, measured_landmark_y
     
     def calc_measurement_cov(self, landmark_cov, Q):
+        # pdb.set_trace()
         H = np.identity(2)
         return H @ landmark_cov @ H.T + Q
 
@@ -279,12 +283,33 @@ class Fastslam():
         # self.weights = new_weights
         # print(self.particles)
         # self.particles = [self.particles[i] for i in selected_indices]
-        
+    
+    def set_landmark_to_groundtruth(self, positions, covariance):
+        for particle in self.particles:
+            particle.landmark = deepcopy(positions)
+            particle.landmark_cov = deepcopy(covariance)
+    
+    def set_position_to_groundtruth(self, current_state):
+        for particle in self.particles:
+            particle.state = deepcopy(current_state)
 
 if __name__ == "__main__":
-    num_particles = 3
-    test = Fastslam(num_particles)
-    for i in range(5):
-        test.propagate_all_states(np.array([3, 1.5]) , 1)
-        print(test.particles[0].state)
-        test.resample()
+    # num_particles = 3
+    # test = Fastslam(num_particles)
+    # for i in range(5):
+    #     test.propagate_all_states(np.array([3, 1.5]) , 1)
+    #     print(test.particles[0].state)
+    #     test.resample()
+
+    # testing set_landmark_to_groundtruth
+    # landmark_positions = np.arange(30).reshape(15,2)
+    # landmark_cov = np.arange(60).reshape(15,2,2)
+    state = np.arange(3).reshape(3)
+    test = Fastslam(3)
+    # test.set_landmark_to_groundtruth(landmark_positions, landmark_cov)
+    test.set_position_to_groundtruth(state)
+
+    # self.landmark = np.zeros((15,2))
+    #     #[[1,2],
+    #     #  [2,3]]
+    #     self.landmark_cov = np.zeros((15, 2, 2))

runFASTSlam.py
@@ -13,16 +13,31 @@ groundtruth_data = pd.read_csv("./data/Cleaned_Robot1_Groundtruth.csv")
 landmark_data = pd.read_csv("./data/Landmark_Groundtruth.csv")
 groundtruth_x = groundtruth_data["X"]
 groundtruth_y = groundtruth_data["Y"]
+groundtruth_yaw = groundtruth_data["Orientation"]
+
+landmark_subject = landmark_data["Subject"]
 landmark_x = landmark_data["X"]
 landmark_y = landmark_data["Y"]
+landmark_x_cov = landmark_data["X-std-dev"]
+landmark_y_cov = landmark_data["Y-std-dev"]
 dt = 1
+
+# define landmark state groundtruth and covariance
+landmark_pos = []
+landmark_cov = []
+for i in range(len(landmark_x)):
+    landmark_pos.append([landmark_x[i], landmark_y[i]])
+    landmark_cov.append([[landmark_x_cov[i], 0], [0, landmark_y_cov[i]]])
+landmark_pos = np.array(landmark_pos)
+landmark_cov = np.array(landmark_cov)
+  
 
 # FastSlam
 dataloader = Dataloader.Dataloader("./data/Cleaned_Robot1_Odometry.csv", "./data/Cleaned_Robot1_Measurement.csv", "./data/Cleaned_Robot1_Groundtruth.csv")
 
 # resulting x list
 # resulitng y list
-filter = FASTSlam.Fastslam(1000)
+filter = FASTSlam.Fastslam(200)
 combined_x = []
 combined_y = []
 current_est_landmark_x = []
@@ -40,6 +55,8 @@ for i in tqdm(range(dataloader.len)):
     current_odometry, all_measurements = dataloader.get_next(0)
     u_t_noiseless = np.array([current_odometry["Forward-velocity"], current_odometry["Angular-velocity"]])
     filter.propagate_all_states(u_t_noiseless, dt)
+    # filter.set_position_to_groundtruth(np.array([groundtruth_x[i+10], groundtruth_y[i+10], groundtruth_yaw[i+10]]))
+    filter.set_landmark_to_groundtruth(landmark_pos, landmark_cov)
     # print(filter.particles[0].state)
     filter.reweight_and_update(all_measurements)
     # filter.resample()
@@ -59,16 +76,16 @@ for i in tqdm(range(dataloader.len)):
     final_landmark = combined_landmark
 
 
-# static_ax.scatter(combined_landmark[:, 0], combined_landmark[:,1])
+static_ax.scatter(combined_landmark[:, 0], combined_landmark[:,1])
 # plot all points
-static_fig, static_ax = plt.subplots()
-static_ax.scatter(groundtruth_x, groundtruth_y)
-static_ax.scatter(landmark_x, landmark_y)
-static_ax.scatter(combined_x, combined_y)
-static_ax.set_title("Ground Truth Robot Path and Landmark")
-static_ax.set_xlabel("X (m)")
-static_ax.set_ylabel("Y (m)")
-plt.show()
+# static_fig, static_ax = plt.subplots()
+# static_ax.scatter(groundtruth_x, groundtruth_y)
+# static_ax.scatter(landmark_x, landmark_y)
+# static_ax.scatter(combined_x, combined_y)
+# static_ax.set_title("Ground Truth Robot Path and Landmark")
+# static_ax.set_xlabel("X (m)")
+# static_ax.set_ylabel("Y (m)")
+# plt.show()
 
 # Plot RMS of robot position x, y
 rms_robot = []
@@ -116,6 +133,9 @@ def animate_path(x_positions, y_positions, est_landmark_x, est_landmark_y):
         ln_groundtruth.set_data(x_groundtruth_data, y_groundtruth_data)
         x_path_data.append(x_positions[frame])
         y_path_data.append(y_positions[frame])
+        # pdb.set_trace()
+        # est_landmark_x_data = est_landmark_x_data + est_landmark_x[frame].tolist()
+        # est_landmark_y_data = est_landmark_y_data + est_landmark_y[frame].tolist()
         ln_path.set_data(x_path_data, y_path_data)
         est_landmark_x_data = [est_landmark_x[frame]]
         est_landmark_y_data = [est_landmark_y[frame]]
@@ -129,7 +149,7 @@ def animate_path(x_positions, y_positions, est_landmark_x, est_landmark_y):
 
     return ani
 
-
+# pdb.set_trace()
 ani = animate_path(combined_x, combined_y, current_est_landmark_x, current_est_landmark_y)
 # FFwrite = animation.PillowWriter(fps = 10)
 # ani.save("animation.gif")